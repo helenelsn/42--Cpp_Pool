712ba4e626ad756d03732e497f2490e2b8689849
hlesny <hlesny@student.42.fr>
2023-10-12 15:35:44 +0200

    cpp04/ex03 done

--- Cpp04/ex03/sources/Cure.cpp
+++ Cpp04/ex03/sources/Cure.cpp
@@ -6,7 +6,7 @@
 /*   By: Helene <Helene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:44:23 by Helene            #+#    #+#             */
-/*   Updated: 2023/10/11 17:46:41 by Helene           ###   ########.fr       */
+/*   Updated: 2023/10/12 15:31:58 by Helene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,17 @@ Cure::Cure()
 	std::cout << "Cure: Default constructor" << std::endl;
 }
 
-Cure::Cure(const Cure& other) {
-
+Cure::Cure(const Cure& other) 
+:	AMateria("cure")
+{
 	std::cout << "Cure: Copy constructor" << std::endl;
-	(void) other; // pourquoi??
+	(void) other;
 }
 
 Cure& Cure::operator=(const Cure& other) {
     
 	std::cout << "Cure: Assignment operator" << std::endl;
-	(void) other; // pourquoi??
+	(void) other;
 	return *this;
 }
 
--- Cpp04/ex03/sources/Ice.cpp
+++ Cpp04/ex03/sources/Ice.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Ice.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hlesny <hlesny@student.42.fr>              +#+  +:+       +#+        */
+/*   By: Helene <Helene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:44:46 by Helene            #+#    #+#             */
-/*   Updated: 2023/10/11 19:43:45 by hlesny           ###   ########.fr       */
+/*   Updated: 2023/10/12 15:34:10 by Helene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,17 @@ Ice::Ice(/* args */)
 	std::cout << "Ice: Default constructor" << std::endl;
 }
 
-Ice::Ice(const Ice& other) {
-
+Ice::Ice(const Ice& other)
+:	AMateria("ice")
+{
 	std::cout << "Ice: Copy constructor" << std::endl;
-	(void) other; // pourquoi??
+	(void) other;
 }
 
 Ice& Ice::operator=(const Ice& other) {
     
 	std::cout << "Ice: Assignment operator" << std::endl;
-	(void) other; // pourquoi??
+	(void) other;
 	return *this;
 }
 
